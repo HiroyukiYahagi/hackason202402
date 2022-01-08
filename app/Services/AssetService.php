8de237bbe5dd0984954580a2ea0b10157668fb88
HiroyukiYahagi <yahagi1989@gmail.com>
2022-01-08 23:06:51 +0900

    assets images

--- app/Services/AssetService.php
+++ app/Services/AssetService.php
@@ -19,16 +19,37 @@ class AssetService
   }
 
   public function add($file) {
+    $rowPath = $file->store('assets', 'local');
+    $filePath = Storage::disk('local')->path($rowPath);
+    $dir = explode(".", $rowPath)[0];
+
+    Storage::disk('local')->makeDirectory($dir);
+
+    $image = \Image::make($filePath);
+    $image->orientate();
+    foreach( [1040, 700, 460, 300, 240] as $size ){
+      $image->resize($size, null, function ($constraint) {
+        $constraint->aspectRatio();
+      });
+      
+      $newPath = $dir."/".$size;
+      $image->save( storage_path("app/".$newPath) );
+      $newFile = Storage::disk('local')->get($newPath);
+      Storage::put($newPath, $newFile);
+    }
     $path = $file->store('assets');
     $asset = Asset::create([
       "path" => $path
     ]);
+    Storage::disk('local')->deleteDirectory($dir);
+    Storage::disk('local')->delete($rowPath);
     return $asset;
   }
   public function delete($id) {
     $asset = Asset::find($id);
     $asset->delete();
     \Storage::delete($asset->path);
+    \Storage::deleteDirectory(explode(".", $asset->path)[0]);
     return $asset;
   }
 }
