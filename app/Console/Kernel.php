b220db87745909d36648a10f002ca29392d9baea
HiroyukiYahagi <yahagi1989@gmail.com>
2022-01-09 22:06:23 +0900

    time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(PushMessageCommand::class)->hourlyAt(5);
+        $schedule->command(PushMessageCommand::class)->hourlyAt(30);
     }
 
     /**
