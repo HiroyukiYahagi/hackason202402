5dec474d99c677adfd165f3b2eec06fa80d6ea6d
HiroyukiYahagi <yahagi1989@gmail.com>
2022-01-18 21:04:31 +0900

    image asset compress

--- app/Services/AssetService.php
+++ app/Services/AssetService.php
@@ -19,30 +19,47 @@ class AssetService
   }
 
   public function add($file) {
-    $rowPath = $file->store('assets', 'local');
-    $filePath = Storage::disk('local')->path($rowPath);
-    $dir = explode(".", $rowPath)[0];
+    // $rowPath = $file->store('assets', 'local');
+    // $filePath = Storage::disk('local')->path($rowPath);
+    // $dir = explode(".", $rowPath)[0];
 
-    Storage::disk('local')->makeDirectory($dir);
+    // Storage::disk('local')->makeDirectory($dir);
 
-    $image = \Image::make($filePath);
-    $image->orientate();
-    foreach( [1040, 700, 460, 300, 240] as $size ){
-      $image->resize($size, null, function ($constraint) {
-        $constraint->aspectRatio();
-      });
+    // $image = \Image::make($filePath);
+    // $image->orientate();
+    // foreach( [1040, 700, 460, 300, 240] as $size ){
+    //   $image->resize($size, null, function ($constraint) {
+    //     $constraint->aspectRatio();
+    //   });
       
-      $newPath = $dir."/".$size;
-      $image->save( storage_path("app/".$newPath) );
-      $newFile = Storage::disk('local')->get($newPath);
-      Storage::put($newPath, $newFile);
-    }
+    //   $newPath = $dir."/".$size;
+    //   $image->save( storage_path("app/".$newPath) );
+    //   $newFile = Storage::disk('local')->get($newPath);
+    //   Storage::put($newPath, $newFile);
+    // }
+    // $path = $file->store('assets');
+    // $asset = Asset::create([
+    //   "path" => $path
+    // ]);
+    // Storage::disk('local')->deleteDirectory($dir);
+    // Storage::disk('local')->delete($rowPath);
+
+
     $path = $file->store('assets');
+    $fullPath = Storage::path($path);
+    $dir = explode(".", $path)[0];
+
+    Storage::makeDirectory($dir);
+    $fullDir = Storage::path($dir);
+
+    foreach( [1040, 700, 460, 300, 240] as $size ){
+      exec("convert ".$fullPath." -resize ".$size."x ".$fullDir."/".$size);
+    }
+
     $asset = Asset::create([
       "path" => $path
     ]);
-    Storage::disk('local')->deleteDirectory($dir);
-    Storage::disk('local')->delete($rowPath);
+
     return $asset;
   }
   public function delete($id) {
